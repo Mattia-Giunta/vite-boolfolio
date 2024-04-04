c5d1fb590afdbf63a8208144d49520cd2b4a0d5d
Mattia-Giunta <mattia.giunta.90@hotmail.com>
2024-04-04 13:59:06 +0200

    aggiunto limite di elementi per pagina e aggiunto selezione delle pagina con i rispettivi elementi per pagina

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,28 +1,37 @@
 <script>
 import axios from 'axios';
 
-    export default {
-        name: 'MainApp',
-        data() {
-            return {
-                arrayProjects: []
-                
-            }
-        },
-        methods: {
-            getProject(){
+export default {
+    name: 'MainApp',
+    data() {
+        return {
+            arrayProjects: [],
+            currentPage: '',
+            lastPage: '',
 
-                axios.get( 'http://127.0.0.1:8000/api/test' )
-                    .then( res => {
-                        this.arrayProjects = res.data.projects
-                    })
-            }
-            
-        },
-        mounted() {
-            this.getProject()
-        },
-    }
+        }
+    },
+    methods: {
+        getProject(projectApiPage) {
+
+            axios.get('http://127.0.0.1:8000/api/test',
+                {
+                    params: {
+                        page: projectApiPage
+                    }
+                })
+                .then(res => {
+                    this.arrayProjects = res.data.projects.data
+                    this.currentPage = res.data.projects.current_page
+                    this.lastPage = res.data.projects.last_page
+                })
+        }
+
+    },
+    mounted() {
+        this.getProject(1)
+    },
+}
 
 
 </script>
@@ -33,26 +42,49 @@ import axios from 'axios';
 
         <h3>Main dove stampare i Project</h3>
 
-        <ul>
-            
-            <li v-for="(element,index) in arrayProjects"
-                :key="element.id">
+        <div>
+            <p v-for="(element, index) in arrayProjects" :key="element.id">
+
                 <a href="#">
                     {{ element.title }}
                 </a>
-               
 
-            </li>
-        
-        
-        </ul>
+                <!-- <span class="ms-3">{{ element.type.name }}</span> -->
+
+                <span class="ms-3 bg-success " v-for="(elements, index) in element.technologies" :key="index">
+
+                    {{ elements.name }}
+
+                </span>
+
+            </p>
+
+            <nav aria-label="Page navigation example">
+
+                <ul class="pagination">
+
+                    <li class="page-item" :class="{ 'disabled': currentPage === 1 }">
+                        <button class="page-link" @click="getProject(currentPage - 1)">Previous</button>
+                    </li>
+
+                    <li class="page-item" v-for=" (element, index) in lastPage " :key="index">
+                        <button class="page-link" @click="getProject(element)">{{ element }}</button>
+                    </li>
+
+                    <li class="page-item" :class="{ 'disabled': currentPage === lastPage }">
+                        <button class="page-link" @click="getProject(currentPage + 1)">Next</button>
+                    </li>
+
+                </ul>
+
+            </nav>
+
+        </div>
 
     </main>
 
-    
+
 
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
