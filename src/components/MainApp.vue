6ac7ede5d89beee861e8596c2f24a746b55f787b
Mattia-Giunta <mattia.giunta.90@hotmail.com>
2024-04-04 13:22:40 +0200

    installato bootstrap, svuotato app.vue e creato mainApp.vue, effettuato chiamata axios e effettuato ciclo per vedere i titoli in pagina

---
+++ src/components/MainApp.vue
@@ -0,0 +1,58 @@
+<script>
+import axios from 'axios';
+
+    export default {
+        name: 'MainApp',
+        data() {
+            return {
+                arrayProjects: []
+                
+            }
+        },
+        methods: {
+            getProject(){
+
+                axios.get( 'http://127.0.0.1:8000/api/test' )
+                    .then( res => {
+                        this.arrayProjects = res.data.projects
+                    })
+            }
+            
+        },
+        mounted() {
+            this.getProject()
+        },
+    }
+
+
+</script>
+
+<template>
+
+    <main>
+
+        <h3>Main dove stampare i Project</h3>
+
+        <ul>
+            
+            <li v-for="(element,index) in arrayProjects"
+                :key="element.id">
+                <a href="#">
+                    {{ element.title }}
+                </a>
+               
+
+            </li>
+        
+        
+        </ul>
+
+    </main>
+
+    
+
+</template>
+
+<style scoped>
+
+</style>
